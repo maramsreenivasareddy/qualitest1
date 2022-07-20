cf7e3851c1b2d734a6bb930e307cbf1793330603
sreenu433maram <sreenivasa.maram@zenq.com>
2022-07-20 09:54:16 +0530

    firt commit

---
+++ ucet1.java
@@ -0,0 +1,10 @@
+package mycode3;
+import java.util.*;
+public class ucet1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("wecome");
+	}
+
+}
